d6ff30eba14e9263338674f4696a43907454cc86
gmarraffa <marraffa3ai@gmail.com>
2018-03-30 11:33:49 +0200

    Reso più verboso cheknmake

--- cheknmake.sh
+++ cheknmake.sh
@@ -5,17 +5,21 @@ ERROR=false
 declare -a modules=("redux" "Solidity" "react" "web3function")
 for dr in "${modules[@]}";do
   cd $dr
+  echo "==============$dr=============="
   OUT=$(cat index.puml | $PLANT -syntax)
   if [[ $OUT = *"ERROR"* ]] ; then
     ERROR=true
     echo "$OUT"
   else
+    echo "OK"
     $PLANT index.puml -o ../build/$dr
   fi
   cd ..
 done
+printf "\n\n\n------------RESULT----------\n"
 if $ERROR; then
   echo "ERRORED"
   exit 1
 fi
+echo "SUCCESS"
 exit 0
